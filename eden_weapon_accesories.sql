4cb61549bc52062f67985c5513542664b2b8848f
Pando <gregos1810@gmail.com>
2018-01-16 09:18:12 +0100

    Update eden_weapon_accesories.sql

--- eden_weapon_accesories.sql
+++ eden_weapon_accesories.sql
@@ -1,4 +1,9 @@
-INSERT INTO `items` (`id`, `name`, `label`, `limit`, `rare`, `can_remove`) VALUES (2010, 'yusuf', 'Skin de luxe', -1, 0, 1);
-INSERT INTO `items` (`id`, `name`, `label`, `limit`, `rare`, `can_remove`) VALUES (209, 'grip', 'Poignée', -1, 0, 1);
-INSERT INTO `items` (`id`, `name`, `label`, `limit`, `rare`, `can_remove`) VALUES (208, 'flashlight', 'Lampe', -1, 0, 1);
-INSERT INTO `items` (`id`, `name`, `label`, `limit`, `rare`, `can_remove`) VALUES (207, 'silencieux', 'Silencieux', -1, 0, 1);
+INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES ('silencieux', 'silencieux', -1, 0, 1);
+INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES ('flashlight', 'flashlight', -1, 0, 1);
+INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES ('grip', 'grip', -1, 0, 1);
+INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES ('varmod', 'kit de modif', -1, 0, 1);
+INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES ('clipG', 'magasin grande capacitÃ©', -1, 0, 1);
+INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES ('scope', 'viseur', -1, 0, 1);
+INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES ('radio', 'radio', -1, 0, 1);
+
+
